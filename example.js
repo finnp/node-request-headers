1c2e3750e6ec5bb1529bed4f0511666d4c6f917b
Finn <derfinn@gmail.com>
2014-05-14 18:47:36 +0200

    Simple helper

---
+++ example.js
@@ -0,0 +1,9 @@
+var requestHeaders = require('./index.js');
+
+requestHeaders('http://foaas.herokuapp.com/you/finn/finn', function(headers) {
+  console.dir(headers);
+});
+
+requestHeaders('https://foaas.herokuapp.com/you/finn/finn', function(headers) {
+  console.log('Content type: ' + headers.content_type);
+});
---
+++ index.js
@@ -0,0 +1,21 @@
+var http = require('http');
+var https = require('https');
+var protocol;
+var headers;
+
+module.exports = function(url, cb) {
+  if(url.indexOf('https') === 0)
+    protocol = https;
+  else
+    protocol = http;
+
+  var req = protocol.get(url, function(res) {
+    req.abort();
+    headers = {};
+    for(key in res.headers) {
+      var new_key = key.replace(/-/g, '_');
+      headers[new_key] = res.headers[key] 
+    }
+    cb(headers);
+  });
+};
